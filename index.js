04d204adcc5b58567e7d54e32aec2d839bde07c0
arkartika <71867729+arkartika@users.noreply.github.com>
2020-12-10 20:36:12 +0700

    menambah javascript

---
+++ index.js
@@ -0,0 +1,27 @@
+let formLogin = document.getElementById("formLogin")
+formLogin.addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+let bodyForm = document.getElementById("bodyForm");
+let button = document.querySelector('button[data-toggle="modal"]');
+let modalSuccess =  document.querySelector('.success');
+let modalInvalid =  document.querySelector('.invalid');
+
+button.addEventListener('click', function() {
+    let username = document.forms["login"]["username"].value;
+    let password = document.forms["login"]["password"].value;
+
+    if (username == "pweb_C" && password == "123123") {
+        modalSuccess.classList.add('show');
+        modalInvalid.classList.remove('show');
+        bodyForm.style.backgroundColor = "green";
+    } else {
+        modalInvalid.classList.add('show');
+        bodyForm.style.backgroundColor = "white";
+    }
+});
+
+modalSuccess.addEventListener('click', function() {
+    this.classList.remove('show');
+});
